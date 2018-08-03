5e9633008e445691c693a0b3c1813fcd3d2ed272
liyiheng <liyihenggnehiyil@gmail.com>
2018-08-03 16:18:09 +0800

    Create main.rs

---
+++ code/rust/color_text/main.rs
@@ -0,0 +1,7 @@
+ mod color;
+ use color::Dye;
+
+ fn main(){
+     let red = color::color("red".to_string(),color::RED);
+     println!("color:\n\t{}\n\t{}\n\t{}\n",red,"green".green(),"yellow".color(color::YELLOW));
+ }
