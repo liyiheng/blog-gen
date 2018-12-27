7ba143a9a7d3dd09f0830a21d17a6c5b604799e1
liyiheng <liyihenggnehiyil@gmail.com>
2018-12-27 10:17:52 +0800

    Hello leetcode

--- code/leetcode/src/bin/two_sum.rs
+++ code/leetcode/src/bin/two_sum.rs
@@ -15,8 +15,32 @@ impl Solution {
             let i2 = num_map.get(&v2);
             if let Some(t) = i2 {
                 if *t != i as i32 {
-                    println!("i:{},t:{}", i, t);
                     return vec![i as i32, *t];
+                }
+            }
+            num_map.insert(v, i as i32);
+        }
+        return vec![];
+    }
+
+    // Edition 2015, without Non-lexical lifetime
+    pub fn two_sum_2015(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut num_map: std::collections::HashMap<i32, i32> = std::collections::HashMap::default();
+        let numbers = 0..;
+        for i in numbers.take(nums.len()) {
+            let v = nums[i];
+            let v2 = target - v;
+            let i2 = {
+                let tmp: Option<&i32> = num_map.get(&v2);
+                if tmp.is_some() {
+                    Some(*tmp.unwrap())
+                } else {
+                    None
+                }
+            };
+            if let Some(t) = i2 {
+                if t != i as i32 {
+                    return vec![i as i32, t];
                 }
             }
             num_map.insert(v, i as i32);
