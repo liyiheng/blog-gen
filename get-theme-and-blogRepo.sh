8bc393cf35fd043113550b3af4f3e0fa46302dbc
liyiheng <liyihenggnehiyil@gmail.com>
2019-08-13 15:10:53 +0800

    Theme changed

--- get-theme-and-blogRepo.sh
+++ get-theme-and-blogRepo.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir themes
 cd themes
-git clone https://github.com/kakawait/hugo-tranquilpeak-theme.git
+# git clone https://github.com/kakawait/hugo-tranquilpeak-theme.git
+git clone https://github.com/halogenica/beautifulhugo.git
 cd -
 git clone  git@github.com:XanthusL/blog.git public
