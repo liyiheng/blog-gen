47c127ae52684bc24b2f432cd31c141d0e696342
liyiheng <liyihenggnehiyil@gmail.com>
2020-11-17 13:07:04 +0800

    Leetcode: Same tree

---
+++ code/leetcode/src/bin/same_tree.rs
@@ -0,0 +1,30 @@
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn is_same_tree(
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> bool {
+        p == q
+    }
+}
+struct Solution;
+fn main() {}
