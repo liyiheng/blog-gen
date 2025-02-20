9a069c292b496596836efb38fc2029aeea97fc1a
liyiheng <liyihenggnehiyil@126.com>
2025-02-20 14:35:24 +0800

    Update nvim_init.lua

--- etc/nvim_init.lua
+++ etc/nvim_init.lua
@@ -471,6 +471,7 @@ require("lazy").setup({
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
+				"goimports",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
